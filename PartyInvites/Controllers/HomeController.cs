8a29785f37d3508ee061e1270d78203900650395
dheeraj <djshres@gmail.com>
2019-03-15 17:53:12 +0545

    Initial

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly PartyInvitesContext _context;
+
+        public HomeController(PartyInvitesContext context)
+        {
+            _context = context;
+        }
+        public ViewResult Index()
+        {
+            //int hour = DateTime.Now.Hour;
+            //ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if(ModelState.IsValid)
+            {
+                //Repository.AddResponse(guestResponse);
+                _context.Add(guestResponse);
+                _context.SaveChanges();
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+            }
+          
+        }
+
+        [HttpGet]
+        public ViewResult ListResponses()
+        {
+            //return View(Repository.Responses.Where(r => r.WillAttend == true));
+            return View(_context.GuestResponses.Where(w => w.WillAttend == true));
+         
+        }
+    }
+}
---
+++ PartyInvites/Models/PartyInvitesContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class PartyInvitesContext:DbContext
+    {
+        public PartyInvitesContext(DbContextOptions<PartyInvitesContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<GuestResponse> GuestResponses { get; set; }
+    }
+}
